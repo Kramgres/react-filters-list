6a77e4a360c48a8f6c3a5362d1e827cb51d22ffa
Сергей Маркушин <kramgres@yandex.ru>
2021-07-11 20:17:11 +0700

    finish project (fix filters)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-/*import './App.css';*/
 import './App.scss'
 import {BrowserRouter} from "react-router-dom";
-import {Redirect, Route} from "react-router";
+import {Route} from "react-router";
 import Header from "./components/Header/Header";
 import CommentsContainer from "./components/Comments/CommentsContainer";
 
